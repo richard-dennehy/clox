894397451829fa1b875df9538a754a48e64fa96b
Richard Dennehy <richard.dennehy@outlook.com>
2022-11-25 10:39:04 +0000

    token scanner

--- main.c
+++ main.c
@@ -5,11 +5,36 @@
 #include "vm.h"
 
 static void repl(FreeList* freeList, VM* vm) {
-    assert(!"Not implemented");
+    char line[1024];
+    for (;;) {
+        printf("> ");
+
+        if (!fgets(line, sizeof(line), stdin)) {
+            printf("\n");
+            break;
+        }
+
+        interpret(freeList, vm, line);
+    }
 }
 
 static void runFile(FreeList* freeList, VM* vm, const char* path) {
-    assert(!"Not implemented");
+    FILE* file = fopen(path, "rb");
+    fseek(file, 0L, SEEK_END);
+    size_t fileSize = ftell(file);
+    rewind(file);
+
+    char* source = reallocate(freeList, NULL, 0, fileSize + 1);
+    size_t bytesRead = fread(source, sizeof(char), fileSize, file);
+    source[bytesRead] = '\0';
+
+    fclose(file);
+
+    InterpretResult result = interpret(freeList, vm, source);
+    reallocate(freeList, source, fileSize + 1, 0);
+
+    if (result == INTERPRET_COMPILE_ERROR) exit(65);
+    if (result == INTERPRET_RUNTIME_ERROR) exit(70);
 }
 
 int main(int argc, const char** argv) {
--- scanner.c
+++ scanner.c
@@ -78,6 +78,105 @@ static void skipWhitespace(Scanner* scanner) {
     }
 }
 
+static Token string(Scanner* scanner) {
+    while (peek(scanner) != '"' && !isAtEnd(scanner)) {
+        if (peek(scanner) == '\n') scanner->line++;
+        advance(scanner);
+    }
+
+    if (isAtEnd(scanner)) return errorToken(scanner, "Unterminated string.");
+
+    advance(scanner);
+    return makeToken(scanner, TOKEN_STRING);
+}
+
+static bool isDigit(char c) {
+    return c >= '0' && c <= '9';
+}
+
+static Token number(Scanner* scanner) {
+    while (isDigit(peek(scanner))) advance(scanner);
+
+    if (peek(scanner) == '.' && isDigit(peekNext(scanner))) {
+        advance(scanner);
+
+        while (isDigit(peek(scanner))) advance(scanner);
+    }
+
+    return makeToken(scanner, TOKEN_NUMBER);
+}
+
+static bool isAlpha(char c) {
+    return (c >= 'a' && c <= 'z') ||
+           (c >= 'A' && c <= 'Z') ||
+           c == '_';
+}
+
+static TokenType checkKeyword(Scanner* scanner, uint32_t start, uint32_t length, const char* rest, TokenType type) {
+    if (scanner->current - scanner->start == start + length && memcmp(scanner->start + start, rest, length) == 0) {
+        return type;
+    }
+
+    return TOKEN_IDENTIFIER;
+}
+
+static TokenType identifierType(Scanner* scanner) {
+    switch (*scanner->start) {
+        case 'a':
+            return checkKeyword(scanner, 1, 2, "nd", TOKEN_AND);
+        case 'c':
+            return checkKeyword(scanner, 1, 4, "lass", TOKEN_CLASS);
+        case 'e':
+            return checkKeyword(scanner, 1, 3, "lse", TOKEN_ELSE);
+        case 'f':
+            if (scanner->current - scanner->start > 1) {
+                switch (scanner->start[1]) {
+                    case 'a':
+                        return checkKeyword(scanner, 2, 3, "lse", TOKEN_FALSE);
+                    case 'o':
+                        return checkKeyword(scanner, 2, 1, "r", TOKEN_FOR);
+                    case 'u':
+                        return checkKeyword(scanner, 2, 1, "n", TOKEN_FUN);
+                }
+            }
+            break;
+        case 'i':
+            return checkKeyword(scanner, 1, 1, "f", TOKEN_IF);
+        case 'n':
+            return checkKeyword(scanner, 1, 2, "il", TOKEN_NIL);
+        case 'o':
+            return checkKeyword(scanner, 1, 1, "r", TOKEN_OR);
+        case 'p':
+            return checkKeyword(scanner, 1, 4, "rint", TOKEN_PRINT);
+        case 'r':
+            return checkKeyword(scanner, 1, 5, "eturn", TOKEN_RETURN);
+        case 's':
+            return checkKeyword(scanner, 1, 4, "uper", TOKEN_SUPER);
+        case 't':
+            if (scanner->current - scanner->start > 1) {
+                switch (scanner->start[1]) {
+                    case 'h':
+                        return checkKeyword(scanner, 2, 2, "is", TOKEN_THIS);
+                    case 'r':
+                        return checkKeyword(scanner, 2, 2, "ue", TOKEN_TRUE);
+                }
+            }
+            break;
+        case 'v':
+            return checkKeyword(scanner, 1, 2, "ar", TOKEN_VAR);
+        case 'w':
+            return checkKeyword(scanner, 1, 4, "hile", TOKEN_WHILE);
+    }
+
+    return TOKEN_IDENTIFIER;
+}
+
+static Token identifier(Scanner* scanner) {
+    while (isAlpha(peek(scanner)) || isDigit(peek(scanner))) advance(scanner);
+
+    return makeToken(scanner, identifierType(scanner));
+}
+
 Token scanToken(Scanner* scanner) {
     skipWhitespace(scanner);
     scanner->start = scanner->current;
@@ -85,6 +184,9 @@ Token scanToken(Scanner* scanner) {
     if (isAtEnd(scanner)) return makeToken(scanner, TOKEN_EOF);
 
     char c = advance(scanner);
+    if (isDigit(c)) return number(scanner);
+    if (isAlpha(c)) return identifier(scanner);
+
     switch (c) {
         case '(':
             return makeToken(scanner, TOKEN_LEFT_PAREN);
@@ -109,13 +211,15 @@ Token scanToken(Scanner* scanner) {
         case '*':
             return makeToken(scanner, TOKEN_ASTERISK);
         case '!':
-            return makeToken(scanner, match(scanner, '=') ? TOKEN_NOT_EQUAL: TOKEN_NOT);
+            return makeToken(scanner, match(scanner, '=') ? TOKEN_NOT_EQUAL : TOKEN_NOT);
         case '=':
-            return makeToken(scanner, match(scanner, '=') ? TOKEN_DOUBLE_EQUAL: TOKEN_EQUAL);
+            return makeToken(scanner, match(scanner, '=') ? TOKEN_DOUBLE_EQUAL : TOKEN_EQUAL);
         case '<':
-            return makeToken(scanner, match(scanner, '=') ? TOKEN_LESS_THAN_EQUAL: TOKEN_LESS_THAN);
+            return makeToken(scanner, match(scanner, '=') ? TOKEN_LESS_THAN_EQUAL : TOKEN_LESS_THAN);
         case '>':
-            return makeToken(scanner, match(scanner, '=') ? TOKEN_GREATER_THAN_EQUAL: TOKEN_GREATER_THAN);
+            return makeToken(scanner, match(scanner, '=') ? TOKEN_GREATER_THAN_EQUAL : TOKEN_GREATER_THAN);
+        case '"':
+            return string(scanner);
     }
 
     return errorToken(scanner, "Unexpected character.");
